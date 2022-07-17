eb2ea1fdbe8533e0c882b83a3da8247b0aaeef3b
Luis Arce <luilliarcec@gmail.com>
2022-07-16 20:34:47 -0500

    Add ShieldFactory.php

---
+++ src/ShieldFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Luilliarcec\FilamentShield;
+
+use Illuminate\Support\Collection;
+use Spatie\Permission\PermissionRegistrar;
+
+class ShieldFactory
+{
+    public static function generatePermissions($resource): void
+    {
+        $model = config('permission.models.permission');
+
+        $permissions = collect($resource::permissions())
+            ->reduce(
+                fn($permissions, $suffix) => $permissions->push(
+                    $model::firstOrCreate(
+                        ['name' => $resource::getPermissionName($suffix)],
+                        ['guard_name' => config('filament.auth.guard')]
+                    )
+                ),
+                collect()
+            );
+
+        static::giveSuperAdminPermission($permissions);
+    }
+
+    protected static function giveSuperAdminPermission(string|array|Collection $permissions): void
+    {
+        $model = config('permission.models.permission');
+
+        $role = $model::firstOrCreate(
+            ['name' => 'super_admin'],
+            ['guard_name' => config('filament.auth.guard')]
+        );
+
+        $role->givePermissionTo($permissions);
+
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
+    }
+}
