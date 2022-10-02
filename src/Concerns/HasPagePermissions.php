c59af08111ee2debfcbb8098c7c20518eea25625
Luis Arce <luilliarcec@gmail.com>
2022-10-02 11:07:05 -0500

    Refactor HasPagePermissions concern

--- src/Concerns/HasPagePermissions.php
+++ src/Concerns/HasPagePermissions.php
@@ -3,49 +3,25 @@
 namespace Luilliarcec\FilamentShield\Concerns;
 
 use Filament\Facades\Filament;
-use Illuminate\Support\Str;
+use Filament\Pages\Actions\Action;
 
 trait HasPagePermissions
 {
-    public function booted(): void
+    use HasPermissions;
+
+    public function mount(): void
     {
-        $this->beforeBooted();
-
-        if (! static::canView()) {
-            $this->notify('warning', __('filament-shield::filament-shield.forbidden'));
-
-            $this->beforeShieldRedirects();
-
-            redirect($this->getShieldRedirectPath());
-
-            return;
-        }
-
-        if (method_exists(parent::class, 'booted')) {
-            parent::booted();
-        }
-
-        $this->afterBooted();
+        abort_unless(static::canView(), 403);
     }
 
-    protected function beforeBooted(): void
+    protected static function shouldRegisterNavigation(): bool
     {
-        return;
+        return static::canView() && static::$shouldRegisterNavigation;
     }
 
-    protected function afterBooted(): void
+    protected function configureAction(Action $action): void
     {
-        return;
-    }
-
-    protected function beforeShieldRedirects(): void
-    {
-        return;
-    }
-
-    protected function getShieldRedirectPath(): string
-    {
-        return config('filament.path');
+        $action->authorize(static::canView());
     }
 
     public static function canView(): bool
@@ -56,22 +32,5 @@ trait HasPagePermissions
     public static function permissions(): array|string
     {
         return config('filament-shield.suffixes.page');
-    }
-
-    public static function getResourceName(): string
-    {
-        return Str::of(class_basename(static::class))
-            ->snake()
-            ->lower();
-    }
-
-    public static function getPermissionName(?string $suffix = null): string
-    {
-        return sprintf('%s-%s', static::getResourceName(), static::permissions());
-    }
-
-    protected static function shouldRegisterNavigation(): bool
-    {
-        return static::canView() && static::$shouldRegisterNavigation;
     }
 }
